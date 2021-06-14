b8752717081f60f19e5bff139717c7924041030b
DoubleDebug <duledodicc@gmail.com>
2021-06-14 19:28:42 +0200

    Configured Webpack & Babel

---
+++ README.md
@@ -0,0 +1,22 @@
+# project-manager
+ TypeScript web app for project management
+_______________________________________________________________
+
+* klase
+* Babel ✓
+* moduli
+* webpack ✓
+* funkcionalno programiranje i rad sa nizovima
+    * map
+    * reduce
+    * filter
+    * forEach
+* asinhrono programiranje
+    * fetch
+    * Promise
+* RxJS
+    * switchMap
+    * take
+    * takeUntil
+    * zip
+    * merge
---
+++ dist/main.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+var message = 'Hello world';
+console.log(message);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wcm9qZWN0LW1hbmFnZXIvLi9zcmMvbWFpbi50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBLElBQU0sT0FBTyxHQUFXLGFBQWEsQ0FBQztBQUN0QyxPQUFPLENBQUMsR0FBRyxDQUFDLE9BQU8sQ0FBQyxDQUFDIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBtZXNzYWdlOiBTdHJpbmcgPSAnSGVsbG8gd29ybGQnO1xyXG5jb25zb2xlLmxvZyhtZXNzYWdlKTsiXSwic291cmNlUm9vdCI6IiJ9
